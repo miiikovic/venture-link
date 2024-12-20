c1c81d31ceb2dc29168c92b11a84aee4ac7f3e2f
miiikovic <miiikovic19@gmail.com>
2024-12-20 14:55:19 +0100

    searchbar completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{query?: string} >
+}) {
+
+  const query = (await searchParams).query;
+
   return (
     <>
     <section className="pink_container">
@@ -7,6 +13,7 @@ export default function Home() {
       <p className="sub-heading !max-w-3xl">
         Submit, vote, and compete — your entrepreneurial journey starts here.
       </p>
+      <SearchForm query={query}/>
     </section>
     </>
   );
