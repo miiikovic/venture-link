534d6a0614e2bb0de565f3eaab071e8d8619affe
miiikovic <miiikovic19@gmail.com>
2025-01-14 13:14:18 +0100

    implemented the startup details page with the views counter

--- components/View.tsx
+++ components/View.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import Ping from './Ping'
+import { client } from '@/sanity/lib/client'
+import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/queries'
+import Ping from './Ping';
 
-const View = ({ id }: { id: string }) => {
+const View = async ({ id }: { id: string }) => {
+
+  const { views: totalViews } = await client.withConfig({ useCdn: false}).fetch(STARTUP_VIEWS_QUERY, { id });
   return (
     <div className='view-container'>
         <div className="absolute -top-2 -right-2">
@@ -9,7 +12,7 @@ const View = ({ id }: { id: string }) => {
         </div>
 
         <p className='view-text'>
-            <span className="font-black">100 views</span>
+            <span className="font-black">Views: {totalViews}</span>
         </p>
     </div>
   )
